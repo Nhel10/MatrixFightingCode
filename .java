2460c3563655ee17da53524081a2da3d0d90e301
srock100 <smw4216@yahoo.com>
2015-05-05 09:45:55 -0700

    Update .java

--- .java
+++ .java
@@ -1,11 +1,21 @@
 import java.util.Scanner;
 import java.util.Random;
-public class matrixFightingCode
+public class MatrixFightingCode
 {
   public static Scanner input = new Scanner(System.in);
   public static Random random = new Random();
-  public static vod main(String[] args)
+  public static void main(String[] args)
   {
+    do
+    {
+      String winner = combat();
+      displayWinner(winner);
+      System.out.println("Game over");
+      System.out.println();
+      System.out.println("Play again (y/n)");
+      String again = keyboard.nextString();
+    }while (again.equals("y"));
     
+    input.close();
   }
 }
